61ea1f20a7f9c3a90944725d3a6a4abea0a1b8c6
kyeyunedaniel <kyeyunedaniel0@gmail.com>
2023-07-19 15:18:35 +0300

    first-component

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,6 +16,7 @@ import { createApp } from 'vue';
 const app = createApp({});
 
 import ExampleComponent from './components/ExampleComponent.vue';
+
 app.component('example-component', ExampleComponent);
 
 /**
