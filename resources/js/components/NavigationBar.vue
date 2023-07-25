0c141e74b254e8137472cdeac172b638ac5baccb
kyeyunedaniel <kyeyunedaniel0@gmail.com>
2023-07-25 13:58:59 +0300

    resolving dashbaord clicks

--- resources/js/components/NavigationBar.vue
+++ resources/js/components/NavigationBar.vue
@@ -11,7 +11,7 @@
         theme="dark" app v-model="drawerOpen">
         <!-- Drawer content here -->
         <v-list color="transparent">
-          <v-list-item prepend-icon="mdi-view-dashboard" title="Dashboard"></v-list-item>
+         <v-list-item prepend-icon="mdi-view-dashboard" title="Dashboard"></v-list-item>
           <v-list-item prepend-icon="mdi-account-box" title="Account"></v-list-item>
           <v-list-item prepend-icon="mdi-gavel" title="Admin"></v-list-item>
         </v-list>
