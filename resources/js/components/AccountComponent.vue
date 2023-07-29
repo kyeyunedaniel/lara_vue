d464da1fde5008868ed5f65ed1c5c756b55b40d1
kyeyunedaniel <kyeyunedaniel0@gmail.com>
2023-07-29 20:32:39 +0300

    starting on the form

--- resources/js/components/AccountComponent.vue
+++ resources/js/components/AccountComponent.vue
@@ -2,8 +2,82 @@
     Account component new new 
     <br>
     <!-- <v-btn @addingCart="CartCount($event)">Add to cart</v-btn> -->
-    <v-btn @click="CartCount">Add to cart</v-btn>
+    <!-- <v-btn @click="CartCount">Add to cart</v-btn> -->
+    <v-form>
+    <v-container>
+      <v-row>
+        <v-col
+          cols="12"
+          sm="6"
+        >
+          <v-text-field
+            label="Your product or service"
+            model-value="Grocery delivery"
+            hint="For example, flowers or used cars"
+          ></v-text-field>
+        </v-col>
 
+        <v-col
+          cols="12"
+          sm="6"
+        >
+          <v-text-field
+            label="Your landing page"
+            hint="www.example.com/page"
+            persistent-hint
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+          sm="6"
+        >
+          <v-text-field
+            label="Your product or service"
+            model-value="Grocery delivery"
+            hint="For example, flowers or used cars"
+            variant="solo"
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+          sm="6"
+        >
+          <v-text-field
+            label="Your landing page"
+            hint="www.example.com/page"
+            persistent-hint
+            variant="solo"
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+          sm="6"
+        >
+          <v-text-field
+            label="Your product or service"
+            model-value="Grocery delivery"
+            hint="For example, flowers or used cars"
+            variant="outlined"
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+          sm="6"
+        >
+          <v-text-field
+            label="Your landing page"
+            hint="www.example.com/page"
+            persistent-hint
+            variant="outlined"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
 </template>
 
 <script>
