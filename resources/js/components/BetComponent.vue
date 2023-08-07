27335b22df0cccddd48c343793af29a961535806
kyeyunedaniel <kyeyunedaniel0@gmail.com>
2023-08-07 21:12:04 +0300

    change

--- resources/js/components/BetComponent.vue
+++ resources/js/components/BetComponent.vue
@@ -1,7 +1,7 @@
 
 
 <template>
-    <v-container>
+  <v-container>
     <v-row align="center" justify="center">
       <v-col cols="auto">
         <v-btn size="x-small">Extra small Button</v-btn>
@@ -25,71 +25,68 @@
     </v-row>
   </v-container>
 
-     <div class="row justify-content-center">
-       We are in the vue Bet Component
+  <div class="row justify-content-center">
+    We are in the vue Bet Component
 
     <table class="table">
-        <thead>
-            <tr>
-            <th scope="col">#</th>
-            <th scope="col">Name</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="(product,index) in all_products" :key="product.id">
-            <td scope="row">{{ index+1 }}</td>
-            <td>{{ product.name }}</td>
-            </tr>
-        </tbody>
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Name</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(product, index) in all_products" :key="product.id">
+          <td scope="row">{{ index + 1 }}</td>
+          <td>{{ product.name }}</td>
+        </tr>
+      </tbody>
     </table>
-    
-    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
-        mounted() {
-            console.log('BetComponent mounted.')
-            // this.getBetApi();
-        }, 
-        data(){
-            return{
-                all_products:{},
-                summary:{},
-            }
+  mounted() {
+    console.log("BetComponent mounted.");
+    // this.getBetApi();
+  },
+  data() {
+    return {
+      all_products: {},
+      summary: {},
+    };
+  },
+  methods: {
+    async getBetApi() {
+      const options = {
+        method: "GET",
+        url: "https://demo-project42818.p.rapidapi.com/catalog/products",
+        params: {
+          skip: "1",
+          limit: "40",
         },
-        methods:{
-            async getBetApi(){
+        headers: {
+          // 34e0f5712msh4298147ecdd4eebp147894jsnd3bd548e9af7
+          // 34e0f5712msh4298147ecdd4eebp
+          "X-RapidAPI-Key": "",
+          "X-RapidAPI-Host": "demo-project42818.p.rapidapi.com",
+        },
+        params: {
+          skip: "1",
+          limit: "40",
+        },
+      };
 
-                const options = {
-                method: 'GET',
-                url: 'https://demo-project42818.p.rapidapi.com/catalog/products',
-                params: {
-                    skip: '1',
-                    limit: '40'
-                },
-                headers: {
-                    // 34e0f5712msh4298147ecdd4eebp147894jsnd3bd548e9af7
-                    // 34e0f5712msh4298147ecdd4eebp
-                    'X-RapidAPI-Key': '',
-                    'X-RapidAPI-Host': 'demo-project42818.p.rapidapi.com'
-                },
-                params:{
-                    skip: '1',
-                    limit: '40'
-                }
-                };
-
-                try {
-                    const response = await axios.request(options);
-                    console.log(response.data);
-                    this.all_products=response.data.products;
-                    this.summary = response.data.summary;
-                } catch (error) {
-                    console.error(error);
-                }
-        }, 
-    }
-}
+      try {
+        const response = await axios.request(options);
+        console.log(response.data);
+        this.all_products = response.data.products;
+        this.summary = response.data.summary;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+};
 </script>
