810f9c7fd05be30cd2ac39745ea68cebf3455fcb
kyeyunedaniel <kyeyunedaniel0@gmail.com>
2023-08-18 17:18:27 +0300

    improve ui

---
+++ database/migrations/2023_08_18_134926_create_police_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('police', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('gender')->nullable();
+            $table->string('citizenship')->nullable();
+            $table->string('occupation')->nullable();
+            $table->string('nin')->nullable();
+            $table->string('marital_status')->nullable();
+            $table->string('residence')->nullable();
+            $table->boolean('payment_status')->default(false);
+            $table->string('transaction_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('police');
+    }
+};
--- resources/js/components/PoliceComponent.vue
+++ resources/js/components/PoliceComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  Account component new new
+  Please provide the information below to complete your application
   <br />
   <!-- <v-btn @addingCart="CartCount($event)">Add to cart</v-btn> -->
   <!-- <v-btn @click="CartCount">Add to cart</v-btn> -->
@@ -11,6 +11,7 @@
             label="First Name"
             hint="Your Religious Name"
             variant="solo"
+            required
           ></v-text-field>
         </v-col>
 
@@ -19,36 +20,34 @@
             label="last Name"
             hint="Your Family Name"
             variant="solo"
+            required
           ></v-text-field>
         </v-col>
 
-        <v-col cols="12" sm="6">
-        <v-select
-          label="Gender"
-          :items="[
-            'Male',
-            'Female'
-          ]"
-          variant="solo"
-        ></v-select>
+        <v-col cols="12" sm="4">
+          <v-select
+            label="Gender"
+            :items="['Male', 'Female']"
+            variant="outlined"
+            required
+          ></v-select>
         </v-col>
-        
-        <v-col cols="12" sm="6">
-        <v-select
-          label="CitizenShip"
-          :items="[
-            'Ugandan',
-            'kanyan',
-            'Tanzanian'
-          ]"
-        ></v-select>
+
+        <v-col cols="12" sm="8">
+          <v-select
+            label="CitizenShip"
+            :items="['Ugandan', 'kanyan', 'Tanzanian']"
+            variant="solo-inverted"
+            required
+          ></v-select>
         </v-col>
 
         <v-col cols="12" sm="6">
           <v-text-field
             label="Occupation"
             hint="Your current Occupation / Job "
-            variant="solo"
+            variant="outlined"
+            required
           ></v-text-field>
         </v-col>
 
@@ -56,37 +55,31 @@
           <v-text-field
             label="Your NIN Number"
             hint="Provide your national nin  number for ugandans"
-            variant="solo"
+            variant="outlined"
           ></v-text-field>
         </v-col>
 
-        <v-col cols="12" sm="6">
-          <!-- <v-text-field
-            label="Marital Status"
-            model-value="Grocery delivery"
-            hint="For example, flowers or used cars"
-            variant="outlined"
-          ></v-text-field> -->
+        <v-col cols="12" sm="4">
           <v-select
-          label="Marital Status"
-          :items="[
-            'Married',
-            'Single',
-            'Deceased Partner'
-          ]"
-          variant="solo"
-          required
-        ></v-select>
+            label="Marital Status"
+            :items="['Married', 'Single', 'Deceased Partner']"
+            variant="outlined"
+            required
+          ></v-select>
         </v-col>
-    <br/>
-        <v-col cols="12">
+
+        <v-col cols="12" sm="8">
           <v-text-field
             label="Residence"
             hint="Current Place of Stay"
             persistent-hint
             variant="outlined"
+            v-model="residence"
             required
           ></v-text-field>
+        </v-col>
+        <br />
+        <v-col cols="12">
           <v-btn variant="elevated" color="blue">Submit</v-btn>
         </v-col>
       </v-row>
@@ -101,15 +94,23 @@ export default {
   },
   data() {
     return {
-      niData: 1,
+      //   niData: 1,
+      first_name: "",
+      last_name: "",
+      gender: "",
+      citizenship: "",
+      occupation: "",
+      nin: "",
+      marital_status: "",
+      residence: "",
+      payment_status: "",
+      transaction_id: "",
     };
   },
 
-  methods: {
-    CartCount() {
-      // console.log('we are trying to emit now')
-      this.$emit("CustomCartEvent", this.niData);
-    },
+  methods: {},
+  mounted() {
+    console.log("hello ");
   },
 };
 </script>
