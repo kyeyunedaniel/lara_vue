45781bfa37c291581491af5d49abac882e27a651
kyeyunedaniel <kyeyunedaniel0@gmail.com>
2023-08-19 12:23:50 +0300

    new commit

--- resources/js/components/PaymentComponent.vue
+++ resources/js/components/PaymentComponent.vue
@@ -4,9 +4,29 @@
     Make Payment
   </v-btn> -->
 
-  <v-form action="" method="post">
+  <!-- <v-form action="https://checkout.flutterwave.com/v3/hosted/pay" method="post">
+    <v-row cols="12" sm="3">
+    <v-text-field name="public_key" value=" hidden readonly >sdasdasd</v-text-field>
+    <v-text-field name="customer[email]" value="jessepinkman@walterwhite.org" readonly>dasdasdas</v-text-field>
+    <v-text-field  readonly>dasdasda</v-text-field>
+    </v-row>
     <v-btn variant="flat" color="teal-darken-1">MAKE THE PAYMENT</v-btn>
-  </v-form>
+  </v-form> -->
+
+  <!-- <form method="POST" action="https://checkout.flutterwave.com/v3/hosted/pay">
+  <div> -->
+    <!-- Your order is ₦3,400 -->
+  <!-- </div>
+  <input type="hidden" name="public_key" value="" />
+  <input type="hidden" name="customer[email]" value="jessepinkman@walterwhite.org" />
+  <input type="hidden" name="customer[name]" value="Jesse Pinkman" />
+  <input type="hidden" name="tx_ref" value="bitethtx-019203" />
+  <input type="hidden" name="amount" value="3400" />
+  <input type="hidden" name="currency" value="NGN" />
+  <input type="hidden" name="meta[token]" value="54" />
+  <input type="hidden" name="redirect_url" value="https://google.com" />
+  <v-btn type="submit" id="start-payment-button">Pay Now</button>
+</form> -->
 </template>
 
 <script>
