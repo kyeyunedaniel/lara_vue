637d2195fead67d899c56e6613d2043e870e3d95
kyeyunedaniel <kyeyunedaniel0@gmail.com>
2023-08-22 10:11:21 +0300

    seeding

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Database\Seeders;
+use App\Models\User;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,5 +14,12 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         //
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('12345678'), // Hashed password
+            // 'is_admin' => true, // You might have an 'is_admin' field in your users table
+        ]);
+
     }
 }
