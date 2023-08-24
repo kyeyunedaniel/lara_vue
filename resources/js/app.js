fffeb13e13c6873b7d60b8388fae2d94572c858b
kyeyunedaniel <kyeyunedaniel0@gmail.com>
2023-08-24 11:53:14 +0300

    nn

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,7 +16,8 @@ import { createVuetify } from 'vuetify'
 import { createRouter, createWebHistory } from 'vue-router' 
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-import DataTable from 'datatables.net-vue3';
+// import DataTable from 'datatables.net-vue3';
+// import store from './store/store'
 
 const vuetify = createVuetify({
   components,
---
+++ resources/js/store/store.js
@@ -0,0 +1,16 @@
+// import vue from '@vitejs/plugin-vue';
+// import Vuex from 'vuex';
+
+// Vue.use(Vuex);
+
+// const store = new Vuex.Store({
+//     state:{
+//         products:[
+//             {name:daniel, price :20000},
+//             {name:ethan, price :50000}
+//         ]
+        
+//     }
+// })
+
+// export default store
